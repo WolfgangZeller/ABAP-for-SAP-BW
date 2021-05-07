0ec9343515332ab1ebd2816f2a6536c5ebec8878
WolfgangZeller <67774684+WolfgangZeller@users.noreply.github.com>
2021-05-07 07:43:46 +0200

    Update DELETE.sql

--- AMDP Transformationsroutinen/DELETE.sql
+++ AMDP Transformationsroutinen/DELETE.sql
@@ -1,3 +1,11 @@
 -- ABAP
 -- DELETE source_package WHERE material = '100000144'.
+-- DELETE source_package WHERE material IS INITIAL'.
+-- DELETE source_package WHERE ( material = '100000144' AND customer = 'BTP' ) OR ( material = '100000145' AND customer = 'PURCH' ).
+-- DELETE source_package WHERE material NOT IN r_range. 
+
+-- SQLScript
 outtab = SELECT * FROM :intab WHERE NOT material = '100000144';
+outtab = SELECT * FROM :intab WHERE material <> '0';
+outtab = SELECT * FROM :intab WHERE NOT (( material = '100000144' AND customer = 'BTP' ) OR ( material = '100000145' AND customer = 'PURCH' ));
+outtab = SELECT * FROM :intab WHERE material NOT IN ( '100000144','100000145','100000146' );                                                                                 
